fcc29b5b02058323362326480099c7e2c3892d6b
Eric Hester <eric.hester@umbrella.tech>
2024-11-24 18:10:19 -0500

    fixed object imports

--- data_transformer.py
+++ data_transformer.py
@@ -1,22 +1,32 @@
-from netboxlabs.diode.sdk.models import Resource
+from diode_sdk.models import VirtualMachine, Device
 
 def transform_and_push_clusters(diode_client, cluster_data):
     for cluster in cluster_data:
-        resource = Resource(
-            name=cluster["name"],
-            resource_type="cluster",
-            properties={"hosts": cluster["hosts"]}
-        )
-        diode_client.publish(resource)
+        # Transform each host in the cluster into a Device
+        for host in cluster["hosts"]:
+            device = Device(
+                name=host["name"],
+                site="Default Site",  # Replace with the appropriate site name
+                role="host",          # Replace with the appropriate role
+                manufacturer="VMware",  # Assuming VMware hosts
+                model="ESXi",          # Replace with appropriate model info
+                custom_fields={
+                    "cpus": host["cpus"],
+                    "memory": host["memory"],
+                }
+            )
+            diode_client.publish(device)
 
 def transform_and_push_vms(diode_client, vm_data):
     for vm in vm_data:
-        resource = Resource(
+        # Transform each VM into a VirtualMachine
+        virtual_machine = VirtualMachine(
             name=vm["name"],
-            resource_type="virtual_machine",
-            properties={
-                "interfaces": vm["interfaces"],
-                "disks": vm["disks"]
+            cluster="Default Cluster",  # Replace with the appropriate cluster name
+            role="application-server",  # Replace with the appropriate VM role
+            custom_fields={
+                "interfaces": vm["interfaces"],  # List of NICs
+                "disks": vm["disks"],            # List of disks
             }
         )
-        diode_client.publish(resource)
+        diode_client.publish(virtual_machine)
